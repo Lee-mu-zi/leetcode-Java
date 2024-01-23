194b5f1173c0c12dcaf39864abd544d743479221
lee <li15237845367@163.com>
2024-01-23 23:40:27 +0800

    Update

---
+++ checkTree/src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello");
+        StringBuilder delete = stringBuilder.delete(0, 3);
+        delete.toString()
+        System.out.println(delete);
+    }
+}
+
+class Solution {
+    public boolean checkTree(TreeNode root) {
+
+        return true;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
